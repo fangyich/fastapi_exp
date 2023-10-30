1ebac542270bac86f3b5f7fd45315a97f0c02ca5
fangyi <fangyi.chen@contourcrafting.com>
2023-10-29 21:26:40 -0700

    Form data & websocket.

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import asyncio
 import time
 import typing
 import strawberry
 from typing import Annotated, List, Union
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse, FileResponse
+from fastapi import WebSocket, WebSocketDisconnect
 from pathlib import Path
 from pydantic import BaseModel
 
@@ -75,6 +77,25 @@ project_repo = {"1234": projects[0], "4567": projects[1]}
 async def root():
     return {"message": "Hello World"}
 
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    print("Accpeted connection:", websocket.url)
+    count = 0
+    try:
+        while True:
+            # await websocket.receive_text()
+            data = f"Current count: {count}"
+            print("Sending: ", data, " state: ", websocket.client_state)
+            await websocket.send_text(f"Message text was: {data}")
+            count = count + 1
+            await asyncio.sleep(1)
+    except WebSocketDisconnect:
+        print("Disconnected")
+    except Exception as e:
+        print(">>>> [Exception]: ", e)
+    print("=== Done ===")
+
 @app.get("/projects")
 async def list_projects(
 ) -> List[Project]:
@@ -107,26 +128,45 @@ async def project_sim_file(
 
 @app.post("/project")
 async def create_project(
-    name: Annotated[str, Form()],
+    project_name: Annotated[str, Form()],
+    nozzle_width: Annotated[float, Form()],
+    layer_height: Annotated[float, Form()],
+    printing_speed: Annotated[float, Form()],
+    non_printing_speed: Annotated[float, Form()],
+    extrusion_speed: Annotated[float, Form()],
     image: Annotated[UploadFile, File()],
 ) -> dict:
-    print("Project name: ", name)
+    print("Project name: ", project_name)
     print("Image: ", image.filename)
     print("Type", image.content_type)
-    PROJ_ROOT_DIR = Path("//home/fangyi/Desktop/api_ws")
+    PROJ_ROOT_DIR = Path("/home/fangyi/Desktop/api_ws")
     file_path = PROJ_ROOT_DIR / image.filename
     file_path.write_bytes(await image.read())
     return {"success": True}
 
 @app.post("/uploadfile")
 async def add_design_file(
-    item: Item
-):
+    file: UploadFile,
+) -> StreamingResponse:
     def iterfile():
-        model_file_path = "/home/fangyi/Desktop/api_ws/graphql_app/sample3.json"
+        model_file_path = "/home/fangyi/Desktop/api_ws/fastapi_exp/sample3.json"
         with open(model_file_path, "rb") as f:
             while chunk := f.read(1024 * 1024):
                 yield chunk
 
     headers = {"Content-Disposition": 'attachment; filename="model_data.json"'}
     return StreamingResponse(iterfile(), headers=headers, media_type="application/json")
+
+@app.post("/joint", tags=["joint"])
+async def send_joint_command(
+    name: str,
+    command: str,
+) -> dict:
+    print("Joint: ", name, " ", command)
+    return {"success": True}
+
+@app.get("/joint/{joint_name}", tags=["joint"])
+async def get_joint_status(
+    joint_name: str,
+) -> dict:
+    return {"status": 100}
